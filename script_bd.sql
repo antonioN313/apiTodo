91d0922e54ad94cc85722b9772262a557bba8b6e
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-09-05 15:54:56 -0300

    BD estruturado

--- script_bd.sql
+++ script_bd.sql
@@ -1,4 +1,22 @@
 create database dbApiTodo;
 
 use dbApiTodo;
+/*Tabelas*/
+create table tarefas(
+	id int not null auto_increment,
+    tarefa varchar(30),
+    categoria int,
+    descricao varchar(60),
+    status tinyint
+);
 
+create table categoria(
+	id int not null auto_increment,
+    nome varchar(30)
+);
+ /*primary keys*/
+alter table tarefas add constraint PK_tarefas primary key (id);
+alter table categoria add constraint PK_categoria primary key (id);
+/*Foreign Key*/
+alter table tarefas add CONSTRAINT FK_categoria Foreign key (categoria) references categoria(id); 
+/*Inserir dados de exemplos nas tabelas*/
