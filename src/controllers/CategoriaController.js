bf76ef1c793c0c3bf353763401c48a707e56e915
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-09-07 16:05:22 -0300

    Controllers e Services

---
+++ src/controllers/CategoriaController.js
@@ -0,0 +1,69 @@
+const CategoriaService = require('../services/CategoriaService.js');
+
+module.exports = {
+
+    //buscar todas as categorias
+    buscarTodas: async (req, res) => {
+        let json = {error:'', result:[]};
+
+        let categorias = await CategoriaService.buscartodas();
+        for (let i in categorias) {
+            json.result.push({
+                id: categorias[i].id,
+                nome: categorias[i].nome
+            });
+        }
+        res.json(json);
+    },
+
+    buscarUm: async (req, res) => {
+        let json = {error:'', result:[]};
+
+        let id = req.params.id;
+        let categorias = await CategoriaService.buscarUm(id);
+        if(categorias)
+        {
+            json.result = categorias;
+        }
+        res.json(json);
+    },
+    inserirCategoria: async(req, res) => {
+        let json = {error:'', result:{}};
+
+        let id = req.body.id;
+        let nome = req.body.nome;
+        if (condition) {
+            let CategoriaId = await CategoriaService.inserir(id,nome);
+            json.result = {
+                id: CategoriaId,
+                nome
+            };
+        }else{
+            json.error = 'Campos não enviados';
+        }
+        res.json(json);
+    },
+    alterar: async(req,res) =>{
+        let json = {error:'', result:{}};
+
+        let id = req.body.id;
+        let nome = req.body.nome;
+        if (condition) {
+            await CategoriaService.inserir(id,nome);
+            json.result = {
+                id,
+                nome
+            };
+        }else{
+            json.error = 'Campos não enviados';
+        }
+        res.json(json);
+    },
+    excluir: async(req,res) => {
+        let json = {error:'', result:{}};
+
+        await CategoriaService.excluir(req.params.id);
+        
+        res.json(json);
+    }
+}
